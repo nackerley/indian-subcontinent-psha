a9bf599dea0f61fcc36f6b16e83ea618a14a41aa
ackerlen <ea610@ncf.ca>
2016-01-13 13:33:31 +0100

    Ensure complete enumeration of logic trees. Added a handy script for compressing jobs.
    Former-commit-id: 3ef75f7ed7b052c1cb4a31d3408b15ce238fc94e

---
+++ Jobs/tar_all.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Tar everything, following links, in given directory (defaults to current)
+
+Created on Wed Jan 13 12:52:50 2016
+
+@author: nick
+"""
+
+import os
+import tarfile
+
+
+directory = os.getcwd()
+for sub_dir, _, _ in os.walk(directory):
+    # omit current directory
+    if sub_dir == directory:
+        continue
+
+    # strip path
+    sub_dir = os.path.split(sub_dir)[1]
+
+    # omit hidden directories
+    if sub_dir[0] == '.':
+        continue
+
+    print('Compressing: tar -hzcvf "%s.tar.gz" "%s"' % (sub_dir, sub_dir))
+    with tarfile.open(sub_dir + '.tar.gz', 'w:gz', dereference=True) as tar:
+        tar.add(sub_dir)

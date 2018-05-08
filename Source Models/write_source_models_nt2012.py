4ebf6bb5c995aa5201a8e7e6a9e473eef2e35214
Nick Ackerley <nicholas.ackerley@canada.ca>
2018-05-08 22:22:33 +0000

    Beginning cleanup of polygons. Otherwise smoothed-gridded points fall in multiple zones.

--- Source Models/write_source_models_nt2012.py
+++ Source Models/write_source_models_nt2012.py
@@ -35,7 +35,10 @@ import numpy as np
 import pandas as pd
 import geopandas as gpd
 
+import matplotlib.pyplot as plt
+from shapely.wkt import dumps
 from shapely.geometry import Point
+from descartes import PolygonPatch
 
 from obspy.imaging.beachball import aux_plane
 
@@ -152,7 +155,8 @@ areal2csv(areal_df, AREAL_SOURCE_MODEL_BASE)
 
 mark = time()
 areal_source_model = df2nrml(areal_df, AREAL_SOURCE_MODEL_BASE)
-print('Finished writing NRML source models [%.0f s]' % (time() - mark))
+print('Finished writing areal model to NRML: %s' %
+      pd.to_timedelta(time() - mark, unit='s'))
 
 # %% read logic tree description table
 
@@ -170,7 +174,10 @@ print(reduced_df)
 
 # %% write areal sources to NRML
 
+mark = time()
 df2nrml(areal_collapsed_df, 'areal_collapsed.xml')
+print('Finished writing collapsed areal model to NRML: %s' %
+      pd.to_timedelta(time() - mark, unit='s'))
 
 # %% areal zone data
 
@@ -243,14 +250,15 @@ smoothed_df['geometry'] = [Point(longitude, latitude)
                                   smoothed_df['latitude'])]
 smoothed_df = gpd.GeoDataFrame(smoothed_df, crs='WGS84')
 
-print('Read %d point sources from %d files [%.0f s]\n' %
-      (len(smoothed_df), len(MIN_MAGS)*len(LAYERS_DF), time() - mark))
+print('Read %d point sources from %d files: %s\n' %
+      (len(smoothed_df), len(MIN_MAGS)*len(LAYERS_DF),
+       pd.to_timedelta(time() - mark, unit='s')))
 
 # %% associate points in zones
 
 print('Associate point sources in areal zones with those zones ...')
 # quick, requires no transformations
-start = time()
+mark = time()
 
 smoothed_df['distance'] = np.inf
 smoothed_dfs = []
@@ -261,7 +269,7 @@ for layer_id in LAYERS_DF.index:
         crs='WGS84')
     smoothed_layer_df = gpd.sjoin(
         smoothed_layer_df, areal_layer_df[['a', 'geometry']],
-        how='left')
+        how='left', op='within')
     smoothed_dfs.append(smoothed_layer_df)
 smoothed_df = pd.concat(smoothed_dfs)
 smoothed_df.rename({'index_right': 'zoneid'}, axis=1, inplace=True)
@@ -270,13 +278,37 @@ smoothed_df['in zoneid'] = smoothed_df['zoneid'].copy()
 
 assigned = (~np.isnan(smoothed_df['in zoneid'])) & (smoothed_df['a'] != 0)
 smoothed_df.loc[assigned, 'distance'] = 0
-print('Spatial join accounted for %.0f%% of sources [%.0f s]' %
-      (100*len(smoothed_df[assigned])/len(smoothed_df), time() - start))
+print('Spatial join accounted for %.2f%% of sources: %s' %
+      (100*len(smoothed_df[assigned])/len(smoothed_df),
+       pd.to_timedelta(time() - mark, unit='s')))
+
+# %% no point should be associated with multiple zones
+id_columns = ['latitude', 'longitude', 'layerid', 'mmin']
+duplicated_df = smoothed_df[smoothed_df.duplicated(
+    subset=id_columns, keep=False)].sort_values(id_columns + ['zoneid'])
+if not duplicated_df.empty:
+    point_a = duplicated_df.iloc[0]
+    point_b = duplicated_df.iloc[1]
+    zone_a = areal_df.at[int(point_a.zoneid), 'geometry']
+    zone_b = areal_df.at[int(point_b.zoneid), 'geometry']
+
+    fig, ax = plt.subplots()
+    ax.add_patch(PolygonPatch(zone_a, alpha=0.5))
+    ax.add_patch(PolygonPatch(zone_b, alpha=0.5))
+    ax.scatter(duplicated_df['longitude'], duplicated_df['latitude'])
+    ax.set_xlim((point_a.longitude - 5, point_a.longitude + 5))
+    ax.set_ylim((point_a.latitude - 5, point_a.latitude + 5))
+    ax.set_aspect(1)
+
+    print(int(point_a.zoneid), dumps(zone_a, rounding_precision=2))
+    print(int(point_b.zoneid), dumps(zone_b, rounding_precision=2))
+    print(point_a.longitude, point_a.latitude)
+    raise RuntimeError('Points assigned to multiple zones.')
 
 # %% associate points nearest to zones
 
 print('Find nearest areal zones for remaining points ...')
-start = time()
+mark = time()
 active_areal_df['polygon'] = [
     MyPolygon([geo.point.Point(lat, lon)
                for lat, lon in zip(*zone.geometry.exterior.coords.xy)])
@@ -296,8 +328,9 @@ unassigned_df.loc[:, 'zoneid'] = active_areal_df.loc[
     np.argmin(distances, axis=1), 'zoneid'].values
 unassigned_df.loc[:, 'distance'] = np.amin(distances, axis=1)
 
-print('Nearest zone required for %.0f%% of sources [%.0f s]' %
-      (100*len(unassigned_df)/len(smoothed_df), time() - start))
+print('Nearest zone required for %.0f%% of sources: %s' %
+      (100*len(unassigned_df)/len(smoothed_df),
+       pd.to_timedelta(time() - mark, unit='s')))
 
 smoothed_df = pd.concat((smoothed_df[assigned], unassigned_df))
 
